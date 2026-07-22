571bc2b2c0f6479133d2fa57b220aaceb8dca1f4
Jasur <jasur@gmail.com>
2026-07-22 13:12:22 +0500

    Update website

--- components/Gallery.js
+++ components/Gallery.js
@@ -13,7 +13,7 @@ export default function Gallery({ items = [] }) {
     <section id="gallery" className="mx-auto max-w-7xl px-5 py-20 mt-12">
       
 
- <div className="relative h-[600px] border border-yellow-500/40 overflow-hidden rounded-3xl">
+ <div className="relative w-[900px] ml-42 h-[400px] border border-yellow-500/40 overflow-hidden rounded-3xl">
   <video
     autoPlay
     muted
@@ -25,12 +25,12 @@ export default function Gallery({ items = [] }) {
   </video>
 
   <div className="absolute inset-0 bg-black/55 flex items-center justify-center">
-    <div className="max-w-3xl text-center text-white px-6">
+    <div className="max-w-3xl text-center text-white/25 px-6">
       <h2 className="text-5xl font-bold">
         Bizning Ish Jarayonimiz
       </h2>
 
-      <p className="mt-6 text-lg leading-8 text-gray-200">
+      <p className="mt-6  text-lg leading-8 text-white/25">
         Har bir avtomobilga professional yondashamiz. Zamonaviy uskunalar,
         sifatli materiallar va tajribali mutaxassislar yordamida
         avtomobilingizni mukammal holatga keltiramiz.
